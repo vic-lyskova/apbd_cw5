a939e2e2fc8ca5ea7fc31c996e069e9a92ca59e2
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-09 11:04:30 +0200

    Static class AnimalEndpoints with static method responsible for servicing  animal endpoints. Minimal API

---
+++ Cw5/Cw5/EndPoints/AnimalEndpoints.cs
@@ -0,0 +1,25 @@
+﻿namespace Cw5.EndPoints;
+
+public static class AnimalEndpoints
+{
+    public static void MapAnimalEndpoints (this WebApplication app)
+    {
+        //Minimal API
+        app.MapGet("/animals",  () =>
+        {
+            // 200 - OK
+            return Results.Ok();
+        });
+        app.MapGet("/animals/{id}",  (int id) =>
+        {
+    
+            return Results.Ok(id);
+        });
+
+        app.MapPost("/animals", () =>
+        {
+            // 201 - Created
+            return Results.Created();
+        });
+    }
+}
