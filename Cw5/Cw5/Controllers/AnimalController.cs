25d556f5b5a273a6f0ebca4d5bfca026e39f7349
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-09 11:13:53 +0200

    Class AnimalController responsible for servicing animal endpoints. Controllers

---
+++ Cw5/Cw5/Controllers/AnimalController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cw5.Controllers;
+
+[ApiController]
+[Route("/animals-controller")]
+// [Route("[controller]")
+public class AnimalController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimals()
+    {
+        return Created();
+    }
+}
