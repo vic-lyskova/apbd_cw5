d7cf4c83fb7b487eb52e92e651a5adcbfcf603da
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-09 11:14:28 +0200

    Used controllers

--- Cw5/Cw5/Program.cs
+++ Cw5/Cw5/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -23,6 +24,7 @@ app.UseHttpsRedirection();
 app.MapAnimalEndpoints();
 
 // Controllers
+app.MapControllers();
 
 app.Run();
 
