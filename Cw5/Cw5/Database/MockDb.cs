d40aecd72ec016a9b7a34aa21c6e20782849c1d7
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-15 22:41:31 +0200

    Filled StaticData with data and added this data to MockDb

--- Cw5/Cw5/Database/MockDb.cs
+++ Cw5/Cw5/Database/MockDb.cs
@@ -5,13 +5,12 @@ namespace Cw5.Database;
 public class MockDb
 {
     public List<Animal> Animals { get; set; } = new List<Animal>();
+    public List<Visit> Visits { get; set; } = new List<Visit>();
 
     public MockDb()
     {
-        Animals.Add(new Animal("Dave", "cat", 3.5, "orange"));
-        Animals.Add(new Animal("Mimi", "dog", 25.7, "white"));
-        Animals.Add(new Animal("Jack", "snake", 1.5, "green scales"));
-        Animals.Add(new Animal("Scout", "hamster", 0.9, "beige"));
+        Animals.AddRange(StaticData.Animals);
+        Visits.AddRange(StaticData.Visits);
     }
 
     public Animal? GetAnimalById(int id)
--- Cw5/Cw5/Database/StaticData.cs
+++ Cw5/Cw5/Database/StaticData.cs
@@ -4,10 +4,21 @@ namespace Cw5.Database;
 
 public class StaticData
 {
-    public static List<Animal> Animals = new List<Animal>()
+    public static readonly List<Animal> Animals = new List<Animal>()
     {
         new Animal("Adam", "dog", 5.2, "brown"),
         new Animal("Lucky", "cat", 3.0, "multicolor"),
         new Animal("Popo", "parrot", 1.5, "red"),
+        new Animal("Dave", "cat", 3.5, "orange"),
+        new Animal("Mimi", "dog", 25.7, "white"),
+        new Animal("Jack", "snake", 1.5, "green scales"),
+        new Animal("Scout", "hamster", 0.9, "beige")
+    };
+
+    public static List<Visit> Visits = new List<Visit>()
+    {
+        new Visit("2024-12-05", Animals[0], "visit 0, animal is great and will live a long and happy life", 100),
+        new Visit("2023-10-10", Animals[5], "visit 1, animal is great and will live a long and happy life", 55.5),
+        new Visit("2021-05-08", Animals[2], "visit 2, animal is great and will live a long and happy life", 30),
     };
 }
