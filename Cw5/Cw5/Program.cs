ba5c336f51ed58ddfbc4242be68330e44ab94e54
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-15 23:07:01 +0200

    Deleted auto generated code

--- Cw5/Cw5/Program.cs
+++ Cw5/Cw5/Program.cs
@@ -29,8 +29,3 @@ app.MapAnimalEndpoints();
 app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
