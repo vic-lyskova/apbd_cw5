4d406e4745e723ce8317d9105674b4ef4823f3a9
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-15 23:05:02 +0200

    Added HttpPost request handler for adding visits

--- Cw5/Cw5/Controllers/VisitController.cs
+++ Cw5/Cw5/Controllers/VisitController.cs
@@ -1,3 +1,5 @@
+using Cw5.Database;
+using Cw5.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cw5.Controllers;
@@ -6,5 +8,22 @@ namespace Cw5.Controllers;
 [Route("/visits-controller")]
 public class VisitController : ControllerBase
 {
-    
+    [HttpGet("{id:int}")]
+    public IActionResult GetVisitsOfAnimal(int id)
+    {
+        var visits = new MockDb().GetVisitsOfAnimal(id);
+        if (visits == null)
+        {
+            return NotFound("No visits in the system for animal with id " + id);
+        }
+
+        return Ok(visits);
+    }
+
+    [HttpPost]
+    public IActionResult AddVisit(Visit visit)
+    {
+        new MockDb().Visits.Add(visit);
+        return Created();
+    }
 }
