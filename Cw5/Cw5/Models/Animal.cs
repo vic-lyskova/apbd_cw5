a381ed94fbf27f565ade6db4fcb026e1b91a7b75
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-15 21:33:11 +0200

    Added all animal properties

--- Cw5/Cw5/Models/Animal.cs
+++ Cw5/Cw5/Models/Animal.cs
@@ -4,4 +4,7 @@ public class Animal
 {
     public int ID { get; set; }
     public string Name { get; set; }
+    public string Kategory { get; set; }
+    public double Mass { get; set; }
+    public string FurColor { get; set; }
 }
