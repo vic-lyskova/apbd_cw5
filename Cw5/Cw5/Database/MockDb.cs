2365f1954fc9f8caec354d450d42c70fb38cb563
Victoria Lyskova <s24787@pjwstk.edu.pl>
2024-04-15 21:50:45 +0200

    Filled the StaticData and MockDb with data

--- Cw5/Cw5/Database/MockDb.cs
+++ Cw5/Cw5/Database/MockDb.cs
@@ -8,9 +8,9 @@ public class MockDb
 
     public MockDb()
     {
-        Animals.Add(new Animal());
-        Animals.Add(new Animal());
-        Animals.Add(new Animal());
-        Animals.Add(new Animal());
+        Animals.Add(new Animal("Dave", "cat", 3.5, "orange"));
+        Animals.Add(new Animal("Mimi", "dog", 25.7, "white"));
+        Animals.Add(new Animal("Jack", "snake", 1.5, "green scales"));
+        Animals.Add(new Animal("Scout", "hamster", 0.9, "beige"));
     }
 }
--- Cw5/Cw5/Database/StaticData.cs
+++ Cw5/Cw5/Database/StaticData.cs
@@ -6,8 +6,8 @@ public class StaticData
 {
     public static List<Animal> Animals = new List<Animal>()
     {
-        new Animal(),
-        new Animal(),
-        new Animal(),
+        new Animal("Adam", "dog", 5.2, "brown"),
+        new Animal("Lucky", "cat", 3.0, "multicolor"),
+        new Animal("Popo", "parrot", 1.5, "red"),
     };
 }
